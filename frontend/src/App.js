f0fce1eb2f3b9290c3e126b0c4c32ad3224f8b30
rakshitsawarn <rakshitsawarn@gmail.com>
2025-12-09 23:28:34 +0530

    Frontend setup complete

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/signup";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
